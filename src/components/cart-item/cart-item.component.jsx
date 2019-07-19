bbc7dbc0dfe045a7485ed39a9a5afeb3b28e4e2e
Uriel Hayon <uri.a77aque@gmail.com>
2019-07-19 11:09:37 -0300

    Converted Header, CustomButton, CartDropdown, CartIcon and CartItem components to styled-components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import './cart-item.styles.scss';
+import { CartItemContainer, CartItemImage, CartItemDetailsContainer } from './cart-item.styles';
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-  <div className='cart-item'>
-    <img src={imageUrl} alt='Item'/>
-    <div className='item-details'>
-      <span className='name'>{name}</span>
-      <span className='price'>{quantity} x ${price}</span>
-    </div>
-  </div>
+  <CartItemContainer>
+    <CartItemImage src={imageUrl} alt='Item'/>
+    <CartItemDetailsContainer>
+      <span>{name}</span>
+      <span>{quantity} x ${price}</span>
+    </CartItemDetailsContainer>
+  </CartItemContainer>
 );
 
 export default CartItem;
