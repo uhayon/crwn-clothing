135e9164ee55c9a400a303529b83f100853d9443
Uriel Hayon <uri.a77aque@gmail.com>
2019-11-18 16:40:32 -0300

    Adding performance optimizations to codebase with react lazy, suspense and memo

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
 
-import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
+import Spinner from '../spinner/spinner.component';
 
-const WithSpinner = WrappedComponent => {
-  const Spinner = ({ isLoading, ...otherProps }) => {
-    return isLoading ? (
-      <SpinnerOverlay>
-        <SpinnerContainer />
-      </SpinnerOverlay>
-    ) : (
-      <WrappedComponent {...otherProps} />
-    );
-  };
-
-  return Spinner;
-};
+const WithSpinner = WrappedComponent => ({ isLoading, ...otherProps }) => isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />;
 
 export default WithSpinner;
